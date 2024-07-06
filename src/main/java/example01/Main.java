323253f9c9a431c6b8a7337b1edc356e1198a435
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2024-07-06 10:34:59 -0400

    Example 01 - Adjusting window position and size

---
+++ src/main/java/example01/Main.java
@@ -0,0 +1,18 @@
+package example01;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+    private static final WebDriver webDriver = new FirefoxDriver();
+
+    public static void main(String[] args) {
+        webDriver.manage().window().setPosition(new Point(0,0));
+        webDriver.manage().window().setSize(new Dimension(800,600));
+        webDriver.get("https://www.google.com/");
+        System.out.println("Page title: " + webDriver.getTitle());
+        webDriver.close();
+    }
+}
