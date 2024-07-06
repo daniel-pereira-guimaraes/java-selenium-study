d8e78ba5752ff3a479caac7d163941f1f5041baa
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2024-07-06 10:15:24 -0400

    Showing page title in console

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
+    private static final WebDriver webDriver = new FirefoxDriver();
 
     public static void main(String[] args) {
-        WebDriver webDriver = new FirefoxDriver();
         webDriver.get("https://www.google.com/");
+        showTitle();
+    }
+
+    private static void showTitle() {
+        System.out.println("title: " + webDriver.getTitle());
     }
 }
