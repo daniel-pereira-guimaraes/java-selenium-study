7fa2e9c03cbae49ff1f9f0deacec196a0dc57182
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2024-07-07 11:41:13 -0400

    Example 09 - Interacting with frame

---
+++ src/main/java/example09/InteractionWithFrame.java
@@ -0,0 +1,22 @@
+package example09;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import javax.swing.*;
+
+public class InteractionWithFrame {
+
+    public static void main(String[] args) {
+        var webDriver = new EdgeDriver();
+        try {
+            webDriver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/page.html");
+            JOptionPane.showMessageDialog(null, "Click OK to perform the frame button click");
+            webDriver.switchTo().frame("frame1");
+            webDriver.findElement(By.id("frameButton")).click();
+            JOptionPane.showMessageDialog(null, "Click OK to close all");
+        } finally {
+            webDriver.quit();
+        }
+    }
+}
