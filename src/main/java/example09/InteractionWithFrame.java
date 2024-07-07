3731931ba0037f4d3c5088ded51127c45159cccc
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2024-07-07 11:49:32 -0400

    Example 09 - Interacting with frame - update

--- src/main/java/example09/InteractionWithFrame.java
+++ src/main/java/example09/InteractionWithFrame.java
@@ -11,9 +11,15 @@ public class InteractionWithFrame {
         var webDriver = new EdgeDriver();
         try {
             webDriver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/page.html");
-            JOptionPane.showMessageDialog(null, "Click OK to perform the frame button click");
+
+            JOptionPane.showMessageDialog(null, "Click OK to perform the [Frame button] click");
             webDriver.switchTo().frame("frame1");
             webDriver.findElement(By.id("frameButton")).click();
+
+            JOptionPane.showMessageDialog(null, "Click OK to perform the [Add text] click");
+            webDriver.switchTo().defaultContent();
+            webDriver.findElement(By.id("addText")).click();
+
             JOptionPane.showMessageDialog(null, "Click OK to close all");
         } finally {
             webDriver.quit();
